4d93dfc9a2b5a1a46d3517550385f1246915df0e
Sri Varshith <nvarshith.2007@gmail.com>
2026-05-22 00:25:13 +0530

    added module and delete function

---
+++ go.mod
@@ -0,0 +1,3 @@
+module todo_cli_go
+
+go 1.26.3
--- main.go
+++ main.go
@@ -2,6 +2,12 @@ package main
 
 import "fmt"
 
-func main(){
-	fmt.Println("hello world")
+func main() {
+	todos := Todos{}
+	todos.add("task1")
+	todos.add("task2")
+	fmt.Println(todos)
+	todos.delete(0)
+	fmt.Println(todos)
+
 }
