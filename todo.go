31147bffdd44058acafbcddfcbb57842b59dc2fe
Sri Varshith <nvarshith.2007@gmail.com>
2026-05-21 23:50:36 +0530

    add function

--- todo.go
+++ todo.go
@@ -2,12 +2,20 @@ package main
 
 import "time"
 
-
-type Todo struct{
-	Title string
-	Completed bool
-	CreatedAt time.Time
+type Todo struct {
+	Title       string
+	Completed   bool
+	CreatedAt   time.Time
 	CompletedAt *time.Time
 }
 
+type Todos []Todo
 
+func (todos *Todos) add(title string) {
+	todo := Todo{
+		Title:     title,
+		Completed: false,
+		CreatedAt: time.Now(),
+	}
+	*todos = append(*todos, todo)
+}
