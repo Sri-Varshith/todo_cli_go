268244ff28f40c695217e7edffcc684ff33db97a
Sri Varshith <nvarshith.2007@gmail.com>
2026-05-22 00:35:27 +0530

    added toggle and edit function

--- todo.go
+++ todo.go
@@ -38,9 +38,37 @@ func (todos *Todos) delete(index int) error {
 	if err := t.validateIndex(index); err != nil {
 		return err
 	}
-
+	
 	*todos = append(t[:index], t[index+1:]...)
+	
+	return nil
+	
+}
+
+func (todos *Todos) toggle(index int) error {
+	t:= *todos
+	if err := t.validateIndex(index); err != nil {
+		return err
+	}
+
+	isCompleted := t[index].Completed
+	if !isCompleted{
+		completionTime := time.Now()
+		t[index].CompletedAt = &completionTime 
+	}
+
+	t[index].Completed = !isCompleted
 
 	return nil
+}
 
+func (todos *Todos) edit(index int, title string) error {
+	t:= *todos
+	if err := t.validateIndex(index); err != nil {
+		return err
+	}
+
+	t[index].Title = title
+
+	return nil
 }
